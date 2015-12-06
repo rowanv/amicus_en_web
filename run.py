fd55dd18d9a79e8e6bbd085ef204b5be7e6f1255
rowanv <rrvspam@gmail.com>
2015-12-06 16:39:17 +0100

    Adding spanish translation

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
-from flask.ext.babel import babel
+from flask.ext.babel import Babel
 from config import LANGUAGES
 
 
 app = Flask(__name__)
-app.config.from_pyfile('mysettings.cfg')
 babel = Babel(app)
 
 @babel.localeselector
